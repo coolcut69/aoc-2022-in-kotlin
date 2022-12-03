436c992a02a831dd27de6bf11100c5ea6d7cfd7f
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-03 09:56:35 +0100

    Day 3: initial solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,20 +1,39 @@
 fun main() {
+    val scores = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     fun part1(inputs: List<String>): Int {
-        return 0
+        var score = 0
+        for (rucksack in inputs) {
+            val chunked = rucksack.chunked(rucksack.length / 2)
+            val firstCompartment = chunked.first()
+            val secondsCompartment = chunked.last()
+
+            val intersect = firstCompartment.toList().intersect(secondsCompartment.toList().toSet())
+            score += scores.indexOf(intersect.first()) + 1
+        }
+        return score
     }
 
     fun part2(inputs: List<String>): Int {
-        return 0
+        var score = 0
+        for (rucksack in inputs.chunked(3)) {
+            val intersect =
+                rucksack.get(0).toList().intersect(rucksack.get(1).toList()).intersect(rucksack.get(2).toList())
+            score += scores.indexOf(intersect.first()) + 1
+        }
+        return score
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
-    check(part1(input) == 0)
     println(part1(input))
-    check(part2(input) == 0)
+    check(part1(input) == 8298)
     println(part2(input))
+    check(part2(input) == 2708)
 }
+
+
