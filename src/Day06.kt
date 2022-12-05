118213e329d811c1bebb7c1b6ec90a73d31c1976
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-05 15:21:33 +0100

    prep Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    fun part1(inputs: List<String>): Int {
+        return 0
+    }
+
+    fun part2(inputs: List<String>): Int {
+        return 0
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+
+    check(part1(testInput) == 0)
+
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day06")
+    println(part1(input))
+    check(part1(input) == 0)
+
+    println(part2(input))
+    check(part2(input) == 0)
+}
+
+
