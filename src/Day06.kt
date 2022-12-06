b915f9a23cb07a25a7881c070ea5d439b51ddda8
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-06 07:54:22 +0100

    Day 6: extract function

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,27 +1,22 @@
 fun main() {
 
-    fun part1(input: String): Int {
-        for (i in 0..input.length - 4) {
-            val message = input.substring(i..i + 3)
+    fun check(input: String, size: Int): Int {
+        for (i in 0..input.length - size) {
+            val message = input.substring(i until i + size)
             val groupBy: Map<String, List<String>> = message.chunked(1).groupBy { it.uppercase() }
-            if (groupBy.size == 4){
-//                println(message)
-                return i + 4
+            if (groupBy.size == size) {
+                return i + size
             }
         }
         return 0
     }
 
+    fun part1(input: String): Int {
+        return check(input, 4)
+    }
+
     fun part2(input: String): Int {
-        for (i in 0..input.length - 14) {
-            val message = input.substring(i..i + 13)
-            val groupBy: Map<String, List<String>> = message.chunked(1).groupBy { it.uppercase() }
-            if (groupBy.size == 14){
-//                println(message)
-                return i + 14
-            }
-        }
-        return 0
+        return check(input, 14)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -38,7 +33,6 @@ fun main() {
     check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
     check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
     check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
-
 
     val input = readInput("Day06").first()
     println(part1(input))
