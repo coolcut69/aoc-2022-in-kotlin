3b093af5d7388d130318d01edcf7e08f07333cb1
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-07 11:47:49 +0100

    Day 7: part 2 solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,6 @@
 fun main() {
-    fun part1(inputs: List<String>): Int {
+    fun parrseFileSystem(inputs: List<String>): Directory {
         val rootDirectory = Directory("/", null)
-        val dirList : MutableList<Directory> = ArrayList()
         var currentDir = rootDirectory
         for (action in inputs) {
             if (action.startsWith("$")) {
@@ -20,7 +19,6 @@ fun main() {
                 if (action.startsWith("dir")) {
                     val dirName = action.substringAfter("dir ")
                     val directory = Directory(dirName, currentDir)
-                    dirList.add(directory)
                     currentDir.addDirectory(directory)
                 } else {
                     val size = action.split(" ")[0].toInt()
@@ -29,27 +27,31 @@ fun main() {
                     currentDir.addFile(File(fileName, size))
                 }
             }
-
         }
+        return rootDirectory
+    }
 
-        return rootDirectory.getDirectories().filter { it.getSize() <= 100000 }.sumOf { it.getSize() }
+    fun part1(inputs: List<String>): Int {
+        val rootDirectory = parrseFileSystem(inputs)
+        return rootDirectory.getDirectories(false).filter { it.getSize() <= 100000 }.sumOf { it.getSize() }
     }
 
     fun part2(inputs: List<String>): Int {
-        var score = 0
-        return score
+        val rootDirectory = parrseFileSystem(inputs)
+        val requiredDiskspace = 30000000 - (70000000 - rootDirectory.getSize())
+        return rootDirectory.getDirectories(true).filter { it.getSize() > requiredDiskspace }.minOf { it.getSize() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 95437)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 24933642)
 
     val input = readInput("Day07")
-    println(part1(input))
+//    println(part1(input))
     check(part1(input) == 1723892)
-//    check(part2(input) == 0)
 //    println(part2(input))
+    check(part2(input) == 8474158)
 }
 
 data class Directory(val name: String, val parent: Directory?) {
@@ -64,16 +66,19 @@ data class Directory(val name: String, val parent: Directory?) {
         directories.add(directory)
     }
 
-    fun getDirectory(name: String) : Directory {
-        return directories.find { directory: Directory -> directory.name == name  }!!
+    fun getDirectory(name: String): Directory {
+        return directories.find { directory: Directory -> directory.name == name }!!
     }
 
-    fun getSize() : Int {
+    fun getSize(): Int {
         return files.sumOf { it.size } + directories.sumOf { it.getSize() }
     }
 
-    fun getDirectories(): List<Directory> {
-        return concatenate(directories, directories.flatMap { it.getDirectories() })
+    fun getDirectories(includeRoot: Boolean): List<Directory> {
+        return when (includeRoot) {
+            true -> concatenate(listOf(this), directories, directories.flatMap { it.getDirectories(false) })
+            false -> concatenate(directories, directories.flatMap { it.getDirectories(false) })
+        }
     }
 }
 
