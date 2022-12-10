cab9b770f288483259bec38bd03eccc7cf24fd90
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-10 09:23:02 +0100

    Day 10: part 1 solution

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,16 +1,27 @@
 fun main() {
-    fun moves(inputs: List<String>): List<Move> {
-        val moves = inputs.map {
-            Move(
-                Direction.getDirectionBy(it.split(" ")[0]),
-                it.split(" ")[1].toInt()
-            )
+
+    fun createRegister(inputs: List<String>): Register {
+        val register = Register()
+        for (s in inputs) {
+            if (s == "noop") {
+                register.addIntruction(Instruction("noop", 0))
+            } else {
+                register.addIntruction(Instruction("addx", s.split(" ")[1].toInt()))
+            }
         }
-        return moves
+        return register
     }
 
     fun part1(inputs: List<String>): Int {
-        return 0
+        val register = createRegister(inputs)
+
+        return register.valueAt(20) * 20 +
+                register.valueAt(60) * 60 +
+                register.valueAt(100) * 100 +
+                register.valueAt(140) * 140 +
+                register.valueAt(180) * 180 +
+                register.valueAt(220) * 220
+
     }
 
     fun part2(inputs: List<String>): Int {
@@ -19,12 +30,45 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 13140)
+//    check(part2(testInput) == 0)
 
     val input = readInput("Day10")
 //    println(part1(input))
-    check(part1(input) == 0)
+    check(part1(input) == 15680)
 //    println(part2(input))
 //    check(part2(input) == 0)
+}
+
+class Register() {
+    private val instructions: MutableList<Instruction> = ArrayList()
+    private val registerX: MutableList<Int> = ArrayList()
+
+    init {
+        registerX.add(1)
+    }
+
+    fun xValue(): List<Int> {
+        return registerX
+    }
+
+    fun valueAt(pos: Int): Int {
+        return registerX.subList(0, pos).sum()
+    }
+
+    fun addIntruction(instruction: Instruction) {
+        this.instructions.add(instruction)
+
+        if (instruction.action == "noop") {
+            registerX.add(0)
+        } else {
+            registerX.add(0)
+            registerX.add(instruction.number)
+        }
+    }
+}
+
+data class Instruction(val action: String, val number: Int) {
+
+
 }
