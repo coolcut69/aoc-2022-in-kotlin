d7b5d23448eb574319f8d90488ff7201c9e41673
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-11 14:38:05 +0100

    Day 11: part 2 solution

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,3 +1,5 @@
+import java.math.BigInteger
+
 fun main() {
     fun extracted(inputs: List<String>, borredFactor: Int = 3): List<Monkey> {
         val monkeys: MutableList<Monkey> = ArrayList()
@@ -9,17 +11,26 @@ fun main() {
             val divid = inputs[monkeyId + 3].substringAfter("Test: divisible by ").toInt()
             val trueMonkey = inputs[monkeyId + 4].substringAfter("If true: throw to monkey ").toInt()
             val falseMonkey = inputs[monkeyId + 5].substringAfter("If false: throw to monkey ").toInt()
-            monkeys.add(Monkey(divid, trueMonkey, falseMonkey, numbers.map { it.toLong() }.toMutableList(), operation, borredFactor))
+            monkeys.add(
+                Monkey(
+                    divid,
+                    trueMonkey,
+                    falseMonkey,
+                    numbers.map { it.toLong() }.toMutableList(),
+                    operation,
+                    borredFactor
+                )
+            )
         }
         return monkeys
     }
 
-    fun part1(inputs: List<String>): Long {
+    fun part1(inputs: List<String>, magicNumber: Int): Long {
         val monkeys = extracted(inputs)
 
         repeat(20) {
             for (monkey in monkeys) {
-                val passes = monkey.playRound()
+                val passes = monkey.playRound(magicNumber)
                 for (pas in passes) {
                     val m = monkeys.get(pas.monkey)
                     m.addItem(pas.level)
@@ -30,12 +41,12 @@ fun main() {
         return sorted[0] * sorted[1]
     }
 
-    fun part2(inputs: List<String>): Long {
+    fun part2(inputs: List<String>, magicNumber: Int): BigInteger {
         val monkeys = extracted(inputs, 1)
 
-        repeat(10000) {
+        repeat(10000) { index ->
             for (monkey in monkeys) {
-                val passes = monkey.playRound()
+                val passes = monkey.playRound(magicNumber)
                 for (pas in passes) {
                     val m = monkeys.get(pas.monkey)
                     m.addItem(pas.level)
@@ -43,19 +54,19 @@ fun main() {
             }
         }
         val sorted = monkeys.map { it.inspections() }.sorted().reversed()
-        return sorted[0] * sorted[1]
+        return BigInteger.valueOf(sorted[0]).multiply(BigInteger.valueOf(sorted[1]))
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
-    check(part1(testInput) == 10605L)
-//    check(part2(testInput) == 2713310158)
+    check(part1(testInput, 96577) == 10605L)
+    check(part2(testInput, 96577) == BigInteger.valueOf(2713310158L))
 
     val input = readInput("Day11")
-    println(part1(input))
-//    check(part1(input) == 0)
-    println(part2(input))
-//    check(part2(input) == 0)
+    println(part1(input, 9699690))
+    check(part1(input, 9699690) == 66124L)
+    println(part2(input, 9699690))
+    check(part2(input, 9699690) == BigInteger.valueOf(19309892877L))
 }
 
 data class Monkey(
@@ -69,11 +80,14 @@ data class Monkey(
 
     private var inspections: Long = 0
 
-    fun playRound(): List<Pass> {
+    fun playRound(i: Int): List<Pass> {
         val passes: MutableList<Pass> = ArrayList()
         for (item in items) {
             inspections++
-            val new = applyOperation(item) / borredFactor
+            var new = applyOperation(item) / borredFactor
+
+            new %= i
+
             if (new % divisionTest == 0L) {
                 passes.add(Pass(testTrue, new))
             } else {
