237c02e996f72af48d16d626c8f2013eaa6e1149
cborghmans <chris.borghmans@dpgmedia.be>
2022-12-12 20:08:08 +0100

    Day 9: part 2, solution tnx to https://github.com/tginsberg

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,113 +1,71 @@
-import Direction.*
-import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun main() {
-    fun moves(inputs: List<String>): List<Move> {
-        val moves = inputs.map {
-            Move(
-                Direction.getDirectionBy(it.split(" ")[0]),
-                it.split(" ")[1].toInt()
-            )
-        }
-        return moves
-    }
 
     fun part1(inputs: List<String>): Int {
-        val moves = moves(inputs)
-        val head = Head(0, 0)
-        val tail = Tail(0, 0)
-        val allPositions: MutableSet<Position> = LinkedHashSet()
-        allPositions.add(Position(tail.x, tail.y))
-
-        for (move in moves) {
-            repeat(move.steps) {
-                head.moveDirection(move.direction)
-                if (tail.toFarFrom(head)) {
-                    tail.moveDirection(move.direction, head)
-                    allPositions.add(Position(tail.x, tail.y))
-                }
-            }
-        }
-        return allPositions.size
+        return followPath(2, inputs)
     }
 
     fun part2(inputs: List<String>): Int {
-        val moves = moves(inputs)
-        val tail = Tail(0, 0)
-        val snake = MutableList(10) { Head(0, 0) }
-        val allPositions: MutableSet<Position> = LinkedHashSet()
-        allPositions.add(Position(tail.x, tail.y))
-        for (move in moves) {
-            repeat(move.steps) {
-                snake[0].moveDirection(move.direction)
-                if (tail.toFarFrom(snake[0])) {
-                    tail.moveDirection(move.direction, snake[0])
-                    allPositions.add(Position(tail.x, tail.y))
-                }
-            }
-        }
-        return allPositions.size
+        return followPath(10, inputs)
     }
 
-    // test if implementation meets criteria from the description, like:
+
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 13)
-    check(part2(testInput) == 13)
+    check(part1(testInput) == 88)
+    check(part2(testInput) == 36)
 
     val input = readInput("Day09")
 //    println(part1(input))
     check(part1(input) == 6181)
 //    println(part2(input))
-//    check(part2(input) == 0)
+    check(part2(input) == 2386)
 }
 
-data class Move(val direction: Direction, val steps: Int)
-
-data class Head(var x: Int, var y: Int) {
-    fun moveDirection(direction: Direction) {
+data class Point(val x: Int = 0, val y: Int = 0) {
+    fun move(direction: Char): Point =
         when (direction) {
-            RIGHT -> x++
-            UP -> y++
-            LEFT -> x--
-            DOWN -> y--
+            'U' -> copy(y = y - 1)
+            'D' -> copy(y = y + 1)
+            'L' -> copy(x = x - 1)
+            'R' -> copy(x = x + 1)
+            else -> throw IllegalArgumentException("Unknown Direction: $direction")
         }
-    }
+
+    fun moveTowards(other: Point): Point =
+        Point(
+            (other.x - x).sign + x,
+            (other.y - y).sign + y
+        )
+
+    fun touches(other: Point): Boolean =
+        (x - other.x).absoluteValue <= 1 && (y - other.y).absoluteValue <= 1
+
 }
 
-data class Tail(var x: Int, var y: Int) {
-    private fun moveTo(x: Int, y: Int) {
-        this.x = x
-        this.y = y
-    }
+fun followPath(knots: Int, inputs: List<String>): Int {
+    val headPath: String = parseInputDay9(inputs)
+    val rope = Array(knots) { Point() }
+    val tailVisits = mutableSetOf(Point())
 
-    fun toFarFrom(head: Head): Boolean {
-        return (abs(head.x - this.x) > 1 || abs(head.y - this.y) > 1)
-    }
-
-    fun moveDirection(direction: Direction, head: Head) = when (direction) {
-        RIGHT -> moveTo(head.x - 1, head.y)
-        UP -> moveTo(head.x, head.y - 1)
-        LEFT -> moveTo(head.x + 1, head.y)
-        DOWN -> moveTo(head.x, head.y + 1)
-    }
-}
-
-data class Position(var x: Int, var y: Int)
-
-enum class Direction {
-    RIGHT, UP, LEFT, DOWN;
-
-    companion object {
-        fun getDirectionBy(name: String): Direction = when (name) {
-            "R" -> RIGHT
-            "U" -> UP
-            "L" -> LEFT
-            "D" -> DOWN
-            else ->
-                throw IllegalArgumentException("unknown value")
+    headPath.forEach { direction ->
+        rope[0] = rope[0].move(direction)
+        rope.indices.windowed(2, 1) { (head, tail) ->
+            if (!rope[head].touches(rope[tail])) {
+                rope[tail] = rope[tail].moveTowards(rope[head])
+            }
         }
+        tailVisits += rope.last()
     }
+    return tailVisits.size
 }
 
 
-
+fun parseInputDay9(input: List<String>): String =
+    input.joinToString("") { row ->
+        val direction = row.substringBefore(" ")
+        val numberOfMoves = row.substringAfter(' ').toInt()
+        direction.repeat(numberOfMoves)
+    }
